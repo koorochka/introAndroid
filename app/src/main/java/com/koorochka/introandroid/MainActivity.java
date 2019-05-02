2f26fcabe62e2b210be4e179b9b2962962bbcbfd
Koorochka <artemij@koorochka.ru>
2019-05-02 14:54:26 +0300

    Add test buttton to project

--- app/src/main/java/com/koorochka/introandroid/MainActivity.java
+++ app/src/main/java/com/koorochka/introandroid/MainActivity.java
@@ -2,19 +2,25 @@ package com.koorochka.introandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Inicialisation
         initTextView();
+        initButton();
+
+        // Testing
         setTextView("Intro to Android");
+        button.setText("change text in create");
 
 
     }
@@ -25,5 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void setTextView(String textView) {
         this.textView.setText(textView);
+    }
+
+    public void initButton() {
+        button = findViewById(R.id.testButton);
     }
 }
