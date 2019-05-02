7d80e210edfcd62d364a2b16afddc8ba693f1f89
Koorochka <artemij@koorochka.ru>
2019-05-02 14:47:03 +0300

    Add textView component to the main activity

--- app/src/main/java/com/koorochka/introandroid/MainActivity.java
+++ app/src/main/java/com/koorochka/introandroid/MainActivity.java
@@ -2,12 +2,28 @@ package com.koorochka.introandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initTextView();
+        setTextView("Intro to Android");
+
+
+    }
+
+    public void initTextView() {
+        this.textView = findViewById(R.id.helloText);
+    }
+
+    public void setTextView(String textView) {
+        this.textView.setText(textView);
     }
 }
